e8b289a1c5a940de97ccabe9217d28907783f703
idirjob2023 <idirjob2023@gmail.com>
2024-01-16 22:52:47 -1200

    Prestation Premiere Filtre V1

--- frontend/src/pages/public/Prestations.jsx
+++ frontend/src/pages/public/Prestations.jsx
@@ -22,11 +22,60 @@ const Prestations = () => {
       background-color: red;
     }
     `;
- 
+
+  const Filtereoptions = [
+      {
+        value: '1',
+        label: 'Not Identified',
+      },
+      {
+        value: '2',
+        label: 'Closed',
+      },
+      {
+        value: '3',
+        label: 'Communicated',
+      },
+      {
+        value: '4',
+        label: 'Identified',
+      },
+      {
+        value: '5',
+        label: 'Resolved',
+      },
+      {
+        value: '6',
+        label: 'Cancelled',
+      },
+    ];
+
+
+
   return (
   
   <PrestationS>
-    <section className="Filtres">  Filetes </section>
+    <section className="Filtres">  
+     <Row>
+       <Col>
+        <div className="Filtre">
+        <Select
+          showSearch
+          style={{
+            width: 200,
+          }}
+         placeholder="Search to Select"
+         optionFilterProp="children"
+         
+         options={Filtereoptions}
+        />
+        </div>
+      </Col>
+     </Row>
+    
+    
+
+    </section>
     <section className="autos"> Autos</section>
     
  
